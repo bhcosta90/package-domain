29151c171c67b7210b9ecbc09ebd685b72af6525
Bruno Costa <bruno.costa@superlogica.com>
2022-06-24 13:40:03 -0300

    finalizado

--- src/ValueObjects/EntityObject.php
+++ src/ValueObjects/EntityObject.php
@@ -9,7 +9,7 @@ class EntityObject
     public function __construct(
         public int|string $id,
         public string|object $type,
-        public string $value,
+        public ?string $value = null,
     ) {
         if (gettype($this->type) === 'object') {
             $this->type = get_class($this->type);
