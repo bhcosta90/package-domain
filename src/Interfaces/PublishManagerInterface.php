a6d8ac504424c035a365dac978d235fe85b1e0f8
Bruno Costa <bruno.costa@superlogica.com>
2022-06-28 07:53:21 -0300

    add pubsub

--- src/Interfaces/PublishManagerInterface.php
+++ src/Interfaces/PublishManagerInterface.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Core\Shared\Interfaces;
 
+use Core\Shared\Abstracts\PubSubAbstract;
+
 interface PublishManagerInterface
 {
     /**
-     * @param PubSubInterface[] $events
+     * @param PubSubAbstract[] $events
      * @return void
      */
     public function dispatch(array $events): void;
