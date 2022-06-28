3d3348824a65e020174b0120f09fb96938abc72f
Bruno Costa <bruno.costa@superlogica.com>
2022-06-28 07:55:10 -0300

    add pubsub

--- src/Abstracts/PubSubAbstract.php
+++ src/Abstracts/PubSubAbstract.php
@@ -6,9 +6,9 @@ namespace Core\Shared\Abstracts;
 
 abstract class PubSubAbstract
 {
-    public abstract function name(): void;
+    public abstract function name(): string;
 
-    public abstract function publish(): void;
+    public abstract function publish(): array;
 
     public abstract function subscribe(array $data): void;
 }
